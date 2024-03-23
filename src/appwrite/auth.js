4a87c8bebc45aae7f0d96f3cb1376344d1cfe021
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-03-23 18:56:31 +0530

    appwrite services

---
+++ src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+	client = new Client();
+	account;
+
+	constructor() {
+		this.client
+			.setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+	}
+
+  // appwrite signup user
+	async createAccount({ email, password, name }) {
+		try {
+			const userAccount = await this.account.create(
+				ID.unique(),
+				email,
+				password,
+				name
+			);
+			if (userAccount) {
+				return this.login({ email, password });
+				//call another method
+			} else {
+				return userAccount;
+			}
+		} catch (error) {
+			throw error;
+		}
+	}
+
+  // appwrite login user
+	async login({ email, password }) {
+		try {
+			return await this.account.createEmailSession(email, password);
+		} catch (error) {
+			throw error;
+		}
+	}
+
+  //appwrite check user
+	async getCurrentUser() {
+		try {
+			return await this.account.get();
+		} catch (error) {
+			console.log("Appwrite service :: getCurrentUser :: error", error);
+		}
+
+		return null;
+	}
+
+  // appwrite logout method
+	async logout() {
+		try {
+			await this.account.deleteSessions();
+		} catch (error) {
+			console.log("Appwrite service :: logout :: error", error);
+		}
+	}
+}
+
+const authService = new AuthService();
+
+export default authService;
