bd615760e34d55632dedb634902adc38f3c96df1
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-03-24 15:00:26 +0530

    redux integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { useEffect } from "react";
+import { login, logout } from "./store/authSlice";
+import { Header,Footer } from "./components";
+import { Outlet } from "react-router-dom";
+
 
 function App() {
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
 
-	return (
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.finally(() => setLoading(false));
+	}, []);
+
+	return !loading ? (
 		<>
-			<h1>Mega blog project</h1>
+			<div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+				<div className="w-full block">
+					<Header />
+					<main>
+						<Outlet />{/* outlet */}
+					</main>
+					<Footer />
+				</div>
+			</div>
 		</>
-	);
+	) : null;
 }
 
 export default App;
